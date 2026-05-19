1fb4a4172ab9b44ddff71613650808ec21a1d309
abrahim0111396212-hue <abrahim0111396212@gmail.com>
2026-05-19 11:20:53 +0200

    رفع ملفات التطبيق
    التطبيق الأول

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'sim-erp-v1';
+const urlsToCache = [
+  '.',
+  'index.html',
+  'manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+});
